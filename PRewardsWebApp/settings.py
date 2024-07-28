d8a48cbbfec4d03b0b6e5d2e774a0de7a7febf13
Suraj Mehra <surajmehra2000@gmail.com>
2024-07-28 15:56:32 +0530

    minor changes2

--- PRewardsWebApp/settings.py
+++ PRewardsWebApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4wek*q5t#yqe^_y%c(h6rp&ui&(=g1o=q(ri-t_smw4bdi$&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
